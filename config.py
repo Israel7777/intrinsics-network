51606685b677f7ac131344fe2273e9fbd5b5a8ec
icog-server <se.israel.abebe@gmail.com>
2018-02-15 01:29:47 -0500

    corrected global username and email

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 blender = '/usr/lib.blender/'
-
 shapenet = '/home/icog/israels/intrinsics-network/dataset/ShapeNetCore.v1'
-
 include = '/home/icog/libraries/anaconda2/envs/pyt/bin/anaconda/home/icog/libraries/anaconda2/envs/pyt/bin/python3.5;/usr/include/numpy'
 
 categories = {'car': '02958343', 'chair': '03001627', 'airplane': '02691156', 'sofa': '04256520', 'boat': '04530566', 'motorbike': '03790512', 'flowerpot': '03991062', 'piano': '03928116', 'tower': '04460130', 'bottle': '02876657', 'train': '04468005', 'guitar': '03467517'}
