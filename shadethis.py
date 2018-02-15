98a219284fd4e35c429329e2bcc5e8da64a02b34
Israel777 <se.israel.abebe@gmail.com>
2018-02-15 14:23:51 +0300

    save image

--- shadethis.py
+++ shadethis.py
@@ -6,4 +6,4 @@ val_set = pipeline.IntrinsicDataset('dataset/output/','val',['normals', 'lights'
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, num_workers=4, shuffle=False)
 shader = models.Shader().cuda()
 # pipeline.visualize_shader(shader, val_loader, "saved/a.png",save_raw=True)
-val_losses = pipeline.visualize_decomposer(shader, val_loader, "saved/a.png", 1,save_raw=True)
+val_losses = pipeline.visualize_decomposer_full(shader, val_loader, "saved/a.png", 1,save_raw=True)
