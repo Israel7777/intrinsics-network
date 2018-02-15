1b2e705321333a2e1a48e51c6ca32e8d8473f129
Israel777 <se.israel.abebe@gmail.com>
2018-02-15 14:36:58 +0300

    save image

--- pipeline/visualization.py
+++ pipeline/visualization.py
@@ -117,7 +117,7 @@ def visualize_decomposer(model, loader, save_path, epoch, save_raw = False):
 
     images = []
 
-    for ind, tensors in enumerate(loader.dataset):
+    for ind, tensors in enumerate(loader):
         tensors = [Variable(t.float().cuda(async=True)) for t in tensors]
         inp, mask, refl_targ, depth_targ, shape_targ, lights_targ = tensors
 
