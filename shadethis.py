3abd257b2a475954a537a7317cf7395e75d17d22
Israel777 <se.israel.abebe@gmail.com>
2018-02-16 09:34:18 +0300

    file not found

--- shadethis.py
+++ shadethis.py
@@ -2,7 +2,7 @@ import sys, os, argparse, torch
 import models, pipeline
 
 
-val_set = pipeline.IntrinsicDataset('dataset/output/','val',['normals', 'lights', 'shading'], size_per_dataset=4)
+val_set = pipeline.IntrinsicDataset('dataset/output/','motorbike_val',['normals', 'lights', 'shading'], size_per_dataset=4)
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, num_workers=4, shuffle=False)
 shader = models.Shader().cuda()
 # pipeline.visualize_shader(shader, val_loader, "saved/a.png",save_raw=True)
