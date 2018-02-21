77371b229727758abed26c4dd43a1bd69b6be43c
Israel777 <se.israel.abebe@gmail.com>
2018-02-21 09:11:47 +0300

    testing on car data

--- shadethis.py
+++ shadethis.py
@@ -2,9 +2,11 @@ import sys, os, argparse, torch
 import models, pipeline
 
 
-val_set = pipeline.IntrinsicDataset('dataset/output/','val',['input', 'mask', 'albedo', 'depth', 'normals', 'lights'], size_per_dataset=1)
+val_set = pipeline.IntrinsicDataset('dataset/output/','car_train',['input', 'mask', 'albedo', 'depth', 'normals', 'lights'], size_per_dataset=1)
 print ("is",type(val_set))
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=32, num_workers=4, shuffle=False)
 shader = models.Shader().cuda()
 # pipeline.visualize_shader(shader, val_loader, "saved/a.png",save_raw=True)
-val_losses = pipeline.visualize_decomposer(shader, val_loader, "saved/a.png", 1,save_raw=True)
+for epoch in range(args.num_epochs):
+    save_path = os.path.join('saved/shader/', str(epoch) + '.png')
+    val_losses = pipeline.visualize_decomposer(shader, val_loader, "saved/a.png", 1,save_raw=True)
